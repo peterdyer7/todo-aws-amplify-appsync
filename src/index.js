51cba07c0e876281f09151189b4e16f0dcc9fc6a
peterdyer7 <peter_dyer@hotmail.com>
2019-01-18 16:19:48 -0500

    swapped out the Amplify GraphQL client for the AppSync (Apollo) client

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AWSAppSyncClient from 'aws-appsync';
+import { ApolloProvider } from 'react-apollo';
+import { Rehydrated } from 'aws-appsync-react';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import aws_config from './aws-exports';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new AWSAppSyncClient({
+  url: aws_config.aws_appsync_graphqlEndpoint,
+  region: aws_config.aws_appsync_region,
+  auth: {
+    type: aws_config.aws_appsync_authenticationType,
+    apiKey: aws_config.aws_appsync_apiKey
+  }
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
